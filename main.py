3928bd57a52cf15c14b38a743513a9350219e945
Dmitry <dimalyamin2004@mail.ru>
2020-12-22 17:28:38 +0300

    add some images and level and other

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import os
 import sys
 import pygame
+from math import ceil
 
 pygame.init()
 
 FPS = 60
 WIDTH = 800
-HEIGHT = 500
+HEIGHT = 700
 SCREEN_RECT = (0, 0, WIDTH, HEIGHT)
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
+SPEED = 500
 
 
 def load_image(name, color_key=-1):
-    fullname = os.path.join('data', name)
+    print(name)
     try:
+        fullname = os.path.join('data/', name)
         image = pygame.image.load(fullname).convert()
-    except pygame.error as message:
+    except Exception as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
 
@@ -32,17 +30,92 @@ def load_image(name, color_key=-1):
     return image
 
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+tile_width = tile_height = 100
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+tile_images = {'empty': None, 'wall': pygame.transform.scale(load_image('block.jpg'), (100, 100))}
+player_image = pygame.transform.scale(load_image('pl.png'), (100, 100))
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def go(self):
+        self.rect = self.image.get_rect().move(10, 0)
+
+
 def terminate():
     pygame.quit()
     sys.exit()
 
 
+def load_level(filename):
+    filename = 'levels/' + filename
+    with open(filename, 'r') as map_file:
+        level_map = [line.strip() for line in map_file]
+        max_width = max(map(len, level_map))
+    return list(map(lambda line: line.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '' or level[y][x] == '.':
+                pass
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+class Camera:
+    def __init__(self, field_size):
+        self.dx = 0
+        self.field_size = field_size
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+
+
+player, level_x, level_y = generate_level(load_level('lev_1.txt'))
+camera = Camera((level_x, level_y))
 running = True
+let_go = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            let_go = True
+        if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+            let_go = False
+    if let_go:
+        player.rect.x += ceil(SPEED / FPS)
+    camera.update(player)
+    for sprite in all_sprites:
+        camera.apply(sprite)
     screen.fill(pygame.Color('black'))
+    all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
 terminate()
